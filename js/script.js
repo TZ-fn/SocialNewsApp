10a9bcc8f486166f82bd341b60ef368244a5200e
Tomek <tomekz@vp.pl>
2018-01-12 16:17:29 +0100

    Some basics.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 'use strict';
 window.onload = function () {
+  const submitBtb = document.querySelector('#submitButton'),
+    contentBox = document.querySelector('#content'),
+    addLink = () => {
 
+    };
 };
